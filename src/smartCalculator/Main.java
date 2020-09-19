fbb1010f6d26d046a6914f8ca1606f9a09c43b3b
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-09-19 20:49:48 +0200

    update2
    enter sequence of operations
    sum two numbers
    return one if one input
    or exit

--- src/smartCalculator/Main.java
+++ src/smartCalculator/Main.java
@@ -7,10 +7,15 @@ public class Main {
 	public static final Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		
-		System.out.println(a + b);
+		String input = sc.nextLine();
+		while(!input.equals("/exit")) {
+			if (!input.trim().equals("")) {
+				String inNbr[] = input.split(" ");
+				int sum = inNbr.length == 1 ? Integer.parseInt(inNbr[0]) : (Integer.parseInt(inNbr[0]) + Integer.parseInt(inNbr[1]));
+				System.out.println(sum);
+			}
+			input = sc.nextLine();
+		}
+		System.out.println("Bye!");
 	}
-
 }
