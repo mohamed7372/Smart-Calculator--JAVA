292b73d34b997c080ec3069f10bb8d5343fe3057
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-09-20 19:30:38 +0200

    update5
    invalid expression and unkown comand

--- src/smartCalculator/Main.java
+++ src/smartCalculator/Main.java
@@ -14,25 +14,44 @@ public class Main {
 		while(!input.equals("/exit")) {
 			if (input.equals("/help"))
 				System.out.println("The program calculates the sum of numbers");
+			else if (input.matches("/.*"))
+				System.out.println("Unknown command");
 			else if (!input.trim().equals("")) {
 				Pattern ptRmSpace = Pattern.compile("\\s+");
 				Matcher mt = ptRmSpace.matcher(input);
 				String[] arr = mt.replaceAll(" ").split(" ");
-				System.out.println(arth(arr));
+				arth(arr);
 			}
 			input = sc.nextLine();
 		}
 		System.out.println("Bye!");
 	}
 	
-	static int arth(String ...arr) {
-		int s = Integer.parseInt(arr[0]);
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i].charAt(0) == '-' && arr[i].length() % 2 == 1) 
-				s -= Integer.parseInt(arr[++i]);
-			else 
-				s += Integer.parseInt(arr[++i]);
+	static void arth(String ...arr) {
+		Pattern pt = Pattern.compile("[-+]{0,1}\\d+");
+		Matcher mt = pt.matcher(arr[0]);
+		boolean err = false;
+		int s = 0;
+		if(mt.matches()) {
+			s = Integer.parseInt(arr[0]);
+			for (int i = 1; i < arr.length; i++) {
+				switch (arr[i].charAt(0)) {
+				case '-':
+					if(arr[i].length() % 2 == 1) {
+						s -= Integer.parseInt(arr[++i]);
+						break;
+					}
+				case '+':
+					s += Integer.parseInt(arr[++i]);
+					break;
+				default:
+					err = true;
+				}
+			}
 		}
-		return s;
+		else
+			err = true;
+		
+		System.out.println(err ? "Invalid expression" : s);
 	}
 }
